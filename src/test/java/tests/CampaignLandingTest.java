7256ab30bf78d8d894ad33acab352b6a8dc68295
Shreya-egov <shreya.kumar@egovernments.org>
2026-05-08 14:34:47 +0530

    addinglocator

--- src/test/java/tests/CampaignLandingTest.java
+++ src/test/java/tests/CampaignLandingTest.java
@@ -3,7 +3,7 @@ package tests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import base.BaseTest;
+import base.BaseTest;z
 import pages.CampaignLandingPage;
 
 public class CampaignLandingTest extends BaseTest {

abca595aa69eddeaba60336aa9a54f7525663fde
Shreya-egov <shreya.kumar@egovernments.org>
2026-05-10 19:28:48 +0530

    updating loc

--- src/test/java/pages/DraftCampaignPage.java
+++ src/test/java/pages/DraftCampaignPage.java
@@ -26,7 +26,7 @@ public class DraftCampaignPage {
     private final String campaignDisplayName;
 
     private static final Map<String, String> CAMPAIGN_DISPLAY_NAMES = Map.of(
-            "BEDNET", "Bednet",
+            "BEDNET", "Bednet Campaign",
             "MR-DN", "Seasonal Malaria Chemoprevention (SMC)"
     );
 

4b60d3241b10430c9f7d1ae7edc9d141a90007fd
Shreya-egov <shreya.kumar@egovernments.org>
2026-05-10 19:49:44 +0530

    updating loc

--- src/test/java/pages/AppConfigurationPage.java
+++ src/test/java/pages/AppConfigurationPage.java
@@ -3,14 +3,25 @@ package pages;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
+import utils.ConfigReader;
+
+import java.util.Map;
 
 public class AppConfigurationPage {
 
     private Page page;
 
+    private static final Map<String, String> CAMPAIGN_DISPLAY_NAMES = Map.of(
+            "BEDNET", "Bednet Distribution",
+            "MR-DN", "Seasonal Malaria Chemoprevention (SMC)"
+    );
+
+    private final String campaignDisplayName;
+
     // App configuration elements
     private Locator setUpMobileAppButton;
     private Locator registrationAndDelivery;
+    private Locator deliveryTypeDropdown;
     private Locator closeHousehold;
     private Locator complaints;
     private Locator inventory;
@@ -23,14 +34,26 @@ public class AppConfigurationPage {
 
     public AppConfigurationPage(Page page) {
         this.page = page;
-        this.setUpMobileAppButton = page.locator("#campaign-details-page-button-setup-mobile-app");
-        this.registrationAndDelivery = page.locator("div#setup-mobile-app-card-REGISTRATION").locator("button");
-        this.closeHousehold = page.locator("div#setup-mobile-app-card-CLOSEHOUSEHOLD").locator("button");
-        this.complaints = page.locator("div#setup-mobile-app-card-COMPLAINTS").locator("button");
-        this.inventory = page.locator("div#setup-mobile-app-card-INVENTORY").locator("button");
-        this.stockReconciliation = page.locator("div#setup-mobile-app-card-STOCKRECONCILIATION").locator("button");
-        this.reports = page.locator("div#setup-mobile-app-card-STOCKREPORTS").locator("button");
-        this.permissionHandler = page.locator("div#setup-mobile-app-card-PERMISSIONHANDLER").locator("button");
+        String campaignType = ConfigReader.get("CAMPAIGN_TYPE");
+        this.campaignDisplayName = CAMPAIGN_DISPLAY_NAMES.getOrDefault(campaignType, campaignType);
+        this.deliveryTypeDropdown = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Select an option"));
+        this.setUpMobileAppButton = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Set Up Mobile App"));
+        this.registrationAndDelivery = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Register eligible children")).getByLabel("Configure");
+        this.closeHousehold = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Record households that were")).getByLabel("Configure");
+        this.complaints = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Let field workers log issues")).getByLabel("Configure");
+        this.inventory = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Track and update resources")).getByLabel("Configure");
+        this.stockReconciliation = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Compare reported stock")).getByLabel("Configure");
+        this.reports = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Access summary and detailed")).getByLabel("Configure");
+        this.permissionHandler = page.getByRole(AriaRole.BUTTON,
+                new Page.GetByRoleOptions().setName("Configure").setExact(true));
         this.configureExactButton = page.getByRole(AriaRole.BUTTON,
                 new Page.GetByRoleOptions().setName("Configure").setExact(true));
         this.saveConfigurationButton = page.getByRole(AriaRole.BUTTON,
@@ -51,9 +74,17 @@ public class AppConfigurationPage {
         page.waitForTimeout(2000);
     }
 
+    public void selectDeliveryType() {
+        deliveryTypeDropdown.click();
+        page.waitForTimeout(1000);
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName(campaignDisplayName).setExact(true)).click();
+        page.waitForTimeout(1000);
+    }
+
     public void configureRegistrationAndDelivery() {
         registrationAndDelivery.click();
         page.waitForTimeout(2000);
+        selectDeliveryType();
         clickSaveConfiguration();
     }
 

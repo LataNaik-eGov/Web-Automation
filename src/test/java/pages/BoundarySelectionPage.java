4fe44290a5d330c67d2605fc57139fdfd41fbac4
Shreya-egov <shreya.kumar@egovernments.org>
2026-05-10 13:57:14 +0530

    updating loc

--- src/test/java/pages/BoundarySelectionPage.java
+++ src/test/java/pages/BoundarySelectionPage.java
@@ -11,30 +11,30 @@ public class BoundarySelectionPage {
 
     // Campaign template step elements
     private Locator DefineTarget;
-    private Locator firstlevel;
-    private Locator secondlevel;
-    private Locator thirdlevel;
-    private Locator fourthlevel;
-    private Locator firstoption;
-    private Locator outside;
+    private Locator firstBoundaryLevel;
+    private Locator secondBoundaryLevel;
+    private Locator thirdBoundaryLevel;
+    private Locator fourthBoundaryLevel;
+    private Locator outsideClick;
     private Locator firstCheckbox;
     private Locator thirdCheckbox;
     private Locator nextButton;
+    private Locator submitButton;
 
 
     public BoundarySelectionPage(Page page) {
 
         this.page = page;
-        this.DefineTarget = page.locator("#campaign-details-page-button-selecting-boundaries");
-        this.firstlevel = page.locator(".selecting-boundaries-dropdown input").nth(0);
-        this.secondlevel = page.locator(".selecting-boundaries-dropdown input").nth(1);
-        this.thirdlevel = page.locator(".selecting-boundaries-dropdown input").nth(2);
-        this.fourthlevel = page.locator(".selecting-boundaries-dropdown input").nth(3);
-        this.firstoption = page.locator(".digit-multi-select-dropdown-menuitem");
-        this.outside=page.locator(".dates-description");
-        this.firstCheckbox = page.getByRole(AriaRole.CHECKBOX);
+        this.DefineTarget = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Define Target Areas"));
+        this.firstBoundaryLevel = page.getByRole(AriaRole.TEXTBOX).first();
+        this.secondBoundaryLevel = page.getByRole(AriaRole.TEXTBOX).nth(1);
+        this.thirdBoundaryLevel = page.getByRole(AriaRole.TEXTBOX).nth(2);
+        this.fourthBoundaryLevel = page.getByRole(AriaRole.TEXTBOX).nth(3);
+        this.outsideClick = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Country*"));
+        this.firstCheckbox = page.getByRole(AriaRole.CHECKBOX).first();
         this.thirdCheckbox = page.getByRole(AriaRole.CHECKBOX).nth(2);
-        this.nextButton = page.locator("#campaign-setup-campaign-standalone-setup-campaign-field-primary");
+        this.nextButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Next"));
+        this.submitButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Submit"));
     }
 
     // --- Actions ---
@@ -44,46 +44,41 @@ public class BoundarySelectionPage {
         page.waitForTimeout(1000);
     }
 
-     public void clickfirstlevel() {
-    
-        // firstlevel.first().click();
-        firstlevel.click();
-        firstoption.click();
+    public void clickfirstlevel() {
+        firstBoundaryLevel.click();
+        firstCheckbox.check();
         page.waitForTimeout(1000);
-        outside.click();
+        outsideClick.click();
         page.waitForTimeout(1000);
     }
 
     public void clicksecondlevel() {
-     
-        secondlevel.click();
-        thirdCheckbox.click();
+        secondBoundaryLevel.click();
+        thirdCheckbox.check();
         page.waitForTimeout(1000);
-        outside.click();
+        outsideClick.click();
         page.waitForTimeout(1000);
     }
 
     public void clickthirdlevel() {
-   
-        thirdlevel.click();
+        thirdBoundaryLevel.click();
         page.waitForTimeout(1000);
-        thirdCheckbox.click();
+        thirdCheckbox.check();
         page.waitForTimeout(1000);
-        outside.click();
+        outsideClick.click();
         page.waitForTimeout(1000);
     }
 
     public void clickfourthlevel() {
-
-        fourthlevel.click();
+        fourthBoundaryLevel.click();
         page.waitForTimeout(1000);
-        thirdCheckbox.click();
+        thirdCheckbox.check();
         page.waitForTimeout(1000);
-        outside.click();
-        page.waitForTimeout(1000);
-        nextButton.click();
+        outsideClick.click();
         page.waitForTimeout(1000);
         nextButton.click();
+        page.waitForTimeout(1000);
+        submitButton.click();
         page.waitForTimeout(1000);
     }
 }

a8350db0f574e47fc397b809050041ecb1523c82
Shreya-egov <shreya.kumar@egovernments.org>
2026-06-05 17:37:44 +0530

    console flow

--- src/test/java/pages/AppConfigurationPage.java
+++ src/test/java/pages/AppConfigurationPage.java
@@ -3,7 +3,6 @@ package pages;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
-import com.microsoft.playwright.options.WaitForSelectorState;
 import utils.ConfigReader;
 
 import java.util.Map;
